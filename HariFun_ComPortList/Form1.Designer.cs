41243d306e000e6a25f6bb85391c6eac21fff9d2
Hari Wiguna <hwiguna@gmail.com>
2019-05-25 09:38:23 -0500

    Initial commit

---
+++ HariFun_ComPortList/Form1.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace HariFun_ComPortList
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.output = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // output
+            // 
+            this.output.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.output.Font = new System.Drawing.Font("Consolas", 14.1F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.output.FormattingEnabled = true;
+            this.output.ItemHeight = 55;
+            this.output.Location = new System.Drawing.Point(12, 12);
+            this.output.Name = "output";
+            this.output.Size = new System.Drawing.Size(1585, 334);
+            this.output.TabIndex = 0;
+            this.output.Click += new System.EventHandler(this.output_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1610, 376);
+            this.Controls.Add(this.output);
+            this.Name = "Form1";
+            this.Text = "Com Port List (click list to refresh)";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox output;
+    }
+}
+
---
+++ HariFun_ComPortList/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Management;
+
+namespace HariFun_ComPortList
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void updateList()
+        {
+            // Original code from StackOverflow. Thanks Humudu!
+            // https://stackoverflow.com/questions/2837985/getting-serial-port-information
+            using (var searcher = new ManagementObjectSearcher("SELECT * FROM Win32_PnPEntity WHERE Caption like '%(COM%'"))
+            {
+                var portnames = SerialPort.GetPortNames();
+                var ports = searcher.Get().Cast<ManagementBaseObject>().ToList().Select(p => p["Caption"].ToString());
+
+                var portList = portnames.Select(n => ports.FirstOrDefault(s => s.Contains(n))).ToList();
+
+                // I want the port# to be in front of the description
+                for (int i = 0; i < portList.Count; i++)
+                {
+                    var lastOpenParen = portList[i].LastIndexOf('(');
+                    var lastCloseParen = portList[i].LastIndexOf(')');
+                    var portName = portList[i].Substring(lastOpenParen + 1, lastCloseParen - lastOpenParen - 1);
+                    var portDescription = portList[i].Substring(0, lastOpenParen - 1);
+                    portList[i] = portName + " - " + portDescription;
+                }
+
+                output.DataSource = portList;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            updateList();
+        }
+
+        private void output_Click(object sender, EventArgs e)
+        {
+            updateList();
+        }
+    }
+}
